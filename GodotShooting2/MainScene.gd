d4019dd4a87c58af446f9b59f35f018c2e95e0ba
vivisuke <ntsuda@master.email.ne.jp>
2021-04-17 07:54:27 +0900

    0005 ◎ C 04/17 04/17 0.001│ToDo│Space 押下：自機からミサイル？発射

--- GodotShooting2/MainScene.gd
+++ GodotShooting2/MainScene.gd
@@ -5,12 +5,36 @@ const MOVE_UNIT = 200
 const FIGHTER_LR_SPC = 96
 const MIN_FIGHTER_X = FIGHTER_LR_SPC
 const MAX_FIGHTER_X = SCREEN_WIDTH - FIGHTER_LR_SPC
-const BULLET_DY = -10
+const MISSILE_DY = -10
+
+var Missile = load("res://Missile.tscn")
+
+var missile = null
+var mv = Vector2(0, MISSILE_DY)
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	if missile != null:
+		if missile.position.y < 0:	# 画面上部に出た場合
+			missile.queue_free()
+			missile = null
+		else:
+			var bc = missile.move_and_collide(mv)
+			print(bc)
+			if bc != null:		# 敵機に当たった場合
+				missile.queue_free()
+				missile = null
+				bc.collider.queue_free()
+		pass
+	else:
+		if Input.is_action_pressed("ui_accept"):
+			missile = Missile.instance()
+			missile.position = $Fighter.position
+			#print(missile.position)
+			#bullet.position.x += 6
+			add_child(missile)
 	var dx = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	if dx != 0:
 		$Fighter.position.x += dx * MOVE_UNIT * delta
